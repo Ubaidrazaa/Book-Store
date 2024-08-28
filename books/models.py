6c357f0dd91c0f3ed8fbebfa1b197e7bcf0e69b6
Ubaidrazaa <ubaidraza9962@gmail.com>
2024-08-28 11:31:36 -0700

    change models setting

--- books/models.py
+++ books/models.py
@@ -10,14 +10,11 @@ class book(models.Model):
         MinValueValidator(1), MaxValueValidator(5)])
     author = models.CharField(null=True, max_length=100)
     is_bestselling = models.BooleanField(default=False)
-    slug = models.SlugField(default="", null=False, db_index=True) 
+    slug = models.SlugField(default="", blank=True,  null=False, db_index=True) 
 
     def get_absolute_url(self):
         return reverse("detail-book", args=[self.slug])
-
-    def save(self, *args, **kwargs):
-        self.slug = slugify(self.title)
-        super().save(*args, **kwargs) 
+ 
 
     def __str__(self):
         return f"{self.title} ({self.rating})"
