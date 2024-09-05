58ff28e22addbd46117cd6aff3e8e486be032ec8
Ubaidrazaa <ubaidraza9962@gmail.com>
2024-09-05 14:37:45 -0700

    Use one to one relationship

--- books/admin.py
+++ books/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import book, Author
+from .models import book, Author, Address
 
 # Register your models here.
 
@@ -9,3 +9,4 @@ class bookAdmin(admin.ModelAdmin):
     list_display = ("title", "author", "rating",)
 admin.site.register(book, bookAdmin)
 admin.site.register(Author)
+admin.site.register(Address)
